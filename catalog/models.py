8e4a1290a4f25e3e1500637e320b5bbccdcbe342
Andrew Phoenix <andrew@resonantstudios.ca>
2011-11-06 17:42:57 -0500

    fixed unicode problem with books

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from datetime import datetime  # for tracking Loans
+from datetime import datetime  # for tracking Loan
 
 
 class Book(models.Model):
@@ -8,13 +8,12 @@ class Book(models.Model):
     
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    genre = models.CharField(max_length=20)
+    genre = models.CharField(max_length=20, blank=True)
     #isbn = models.IntegerField()
     
     # This will likely change somewhat based on what kind of 
     # information it's decided to keep on books. Also, hook
     # into an ISBN database for ease of entry.
-
     # It's also possible to generalize this to media by having
     # an "Items" model, then defining models for subsequent
     # media types. For the current issue, this is not necessary.
@@ -28,7 +27,7 @@ class Borrower(models.Model):
     """ Borrower Model - who is taking the book """
 
     name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=255)
+    email = models.EmailField(max_length=255, blank=True)
 
     def __unicode__(self):
         return self.name
@@ -38,9 +37,12 @@ class Loan(models.Model):
 
     """ Loan Model - matching borrowers to books """
 
-    borrower = models.ForeignKey(Borrower)
     book = models.ForeignKey(Book)
+    borrower = models.ForeignKey(Borrower)
     date_lent = models.DateField(default=datetime.now())
+    returned = models.BooleanField(default=False)
 
     def __unicode__(self):
-        return self.book
+        return '{0}'.format(self.book)
+    def return_it(self):
+        return self.returned==True
