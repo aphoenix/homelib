1966b33e7a22de4e1c4421b493d85522a17b5a6e
Andrew Phoenix <andrew@resonantstudios.ca>
2011-11-07 21:27:56 -0500

    getting pages to show proper templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
 
+
 urlpatterns = patterns('catalog.views',
     url(r'^$', 'index'),
-    url(r'^(?P<book_id>\d+)/$', 'detail'),
+    url(r'^book/(?P<book_id>\d+)/$', 'book'),
+    url(r'^borrower/(?P<borrower_id>\d+)/$', 'borrower'),
     )
+
