7143def5892ff57b7aa95c8656b0fcd2f6639214
Andrew Phoenix <andrew@resonantstudios.ca>
2011-11-08 01:43:25 -0500

    changed requests to signout because requests is used a lot for other things?

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('catalog.views',
     url(r'^$', 'index'),
     url(r'^book/$', 'library'),
     url(r'^book/(?P<book_id>\d+)/$', 'book'),
-    url(r'^book/(?P<book_id>\d+)/request/$', 'signout'),
+    url(r'^book/(?P<book_id>\d+)/signout/$', 'signout'),
     url(r'^borrower/(?P<borrower_id>\d+)/$', 'borrower'),
     )
 
