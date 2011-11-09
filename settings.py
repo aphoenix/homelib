4b1640780bbe2bd336f3622b9e615d9dfa5915c8
Andrew Phoenix <andrew.phoenix@gmail.com>
2011-11-08 21:09:26 -0500

    templates beauitified and css:ed

--- settings.py
+++ settings.py
@@ -44,7 +44,7 @@ USE_L10N = True
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    '/home/aphoenix/homelib/codebase/media',
+    '/home/aphoenix/homelib/media',
 )
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -53,7 +53,7 @@ STATICFILES_FINDERS = (
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/aphoenix/homelib/codebase/media'
+MEDIA_ROOT = '/home/aphoenix/homelib/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -83,10 +83,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'codebase.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
-    "/home/aphoenix/homelib/codebase/templates"
+    "/home/aphoenix/homelib/templates"
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
