883717a7a8557a5f1ae1d94c2fbaf6248b79197a
Andrew Phoenix <andrew.phoenix@gmail.com>
2011-11-13 21:17:08 -0500

    removed urlresolvers reverse from imports - no longer used

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from catalog.models import Book, Borrower, Loan
 
